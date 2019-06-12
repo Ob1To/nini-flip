b8bb3558497cac2cb5f3b958096f60edfc4b8d15
Ivan Markov <markov.tennis@gmail.com>
2019-06-12 17:26:04 +0300

    initial commit

---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Card.css'
+
+class Card extends Component {
+    render() {
+        return (
+            <div className='Card-main'>
+                <h1>This is my h1 header</h1>
+                <h2>and this is my h2 header</h2>
+            </div>
+        )
+
+    }
+}
+
+export default Card
